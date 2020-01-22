d7bbd64122a4118fe84210ef9e5cc557c21756c2
Obe Kohei <obe.kohei@ditgroup.jp>
2020-01-22 18:36:20 +0900

    add: firestoreの設定追加。サンプルコードを実装。

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,20 +6,33 @@ export default {
   head: {
     title: process.env.npm_package_name || '',
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        charset: 'utf-8'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      },
       {
         hid: 'description',
         name: 'description',
         content: process.env.npm_package_description || ''
       }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [
+      {
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: '/favicon.ico'
+      }
+    ]
   },
   /*
    ** Customize the progress-bar color
    */
-  loading: { color: '#fff' },
+  loading: {
+    color: '#fff'
+  },
   /*
    ** Global CSS
    */
@@ -27,7 +40,7 @@ export default {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: [],
+  plugins: ['~/plugins/database'],
   /*
    ** Nuxt.js dev-modules
    */
@@ -59,6 +72,16 @@ export default {
     /*
      ** You can extend webpack config here
      */
-    extend(config, ctx) {}
+    extend(config, ctx) {
+      if (ctx.isDev && ctx.isClient) {
+        config.devtool = 'inline-cheap-module-source-map' // <-- ここを足す
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
   }
 }
---
+++ store/index.js
@@ -0,0 +1,26 @@
+import Vuex from 'vuex'
+import { vuexfireMutations, firestoreAction } from 'vuexfire'
+
+// sample store
+const createStore = () => {
+  return new Vuex.Store({
+    state: {
+      users: []
+    },
+    mutations: {
+      ...vuexfireMutations
+    },
+    actions: {
+      setUsersRef: firestoreAction(({ bindFirestoreRef }, ref) => {
+        bindFirestoreRef('users', ref)
+      })
+    },
+    getters: {
+      getUsers: (state) => {
+        return state.users
+      }
+    }
+  })
+}
+
+export default createStore
