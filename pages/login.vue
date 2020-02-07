bab8a4ab71b855ab2f7f55a4c8bc3d337519526a
Obe Kohei <obe.kohei@ditgroup.jp>
2020-02-07 17:44:52 +0900

    add: ログイン画面を作成(途中)

---
+++ pages/login.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div id="firebaseui-auth-container"></div>
+  </div>
+</template>
+
+<script>
+import * as firebaseui from 'firebaseui'
+import firebase from '~/plugins/firebase'
+import 'firebaseui/dist/firebaseui.css'
+
+export default {
+  mounted() {
+    const uiConfig = {
+      signInSuccessUrl: '/',
+      signInOptions: [
+        firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID,
+        firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID
+      ]
+    }
+
+    const ui = new firebaseui.auth.AuthUI(firebase.auth())
+    ui.start('#firebaseui-auth-container', uiConfig)
+  }
+}
+</script>
--- store/index.js
+++ store/index.js
@@ -1,26 +1,5 @@
-import Vuex from 'vuex'
-import { vuexfireMutations, firestoreAction } from 'vuexfire'
+import { vuexfireMutations } from 'vuexfire'
 
-// sample store
-const createStore = () => {
-  return new Vuex.Store({
-    state: {
-      users: []
-    },
-    mutations: {
-      ...vuexfireMutations
-    },
-    actions: {
-      setUsersRef: firestoreAction(({ bindFirestoreRef }, ref) => {
-        bindFirestoreRef('users', ref)
-      })
-    },
-    getters: {
-      getUsers: (state) => {
-        return state.users
-      }
-    }
-  })
+export const mutations = {
+  ...vuexfireMutations
 }
-
-export default createStore
